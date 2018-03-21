aec10bca951e401d3408eff2f054cb0f66de6a77
Rodolfo Dominguez <roddom67@gmail.com>
2018-03-21 09:43:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,62 @@
-<!doctype html><html lang="es">  <head>    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    <meta name="description" content="">    <meta name="author" content="">    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">	<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">	<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">	<link rel="manifest" href="/manifest.json">	<meta name="msapplication-TileColor" content="#ffffff">	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">	<meta name="theme-color" content="#ffffff">    <title>Estoy relajado</title>    <!-- Bootstrap core CSS -->    <link href="css/bootstrap.min.css" rel="stylesheet">    <!-- Custom styles for this template -->    <link href="css/style.css" rel="stylesheet">  </head>  <body>    <main role="main" class="container-fluid" id="cambiante">      <div class="starter-template">        <h1><span>Yo estoy... relajado</span></h1>        <p><a href="#comienzo" title="Comienzo" class="btn">Comienzo...</a></p>      </div>      <div id="comienzo" class="hidden">        <p class="lead">Estando con los ojos abiertos o cerrados, y sin hablar, me concentro en cómo estoy respirando. </p>      </div>            <div id="estoyRelajado" class="hidden">      	<div id="yoestoy">      		<p class="lead">Ahora, con cada respiración pienso: <span>"Yo estoy..."</span></p>      	</div>      	<div id="yoestoy" class="hidden">      		<p class="lead">"...relajado"</p>      	</div>      </div>    </main><!-- /.container -->    <script src="js/jquery.min.js"></script>    <script src="js/bootstrap.min.js"></script>    <script src="js/script.js"></script>  </body></html>
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <title>Estoy relajado</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <main role="main" class="container-fluid" id="cambiante">
+
+      <div class="starter-template">
+        <h1><span>Yo estoy... relajado</span></h1>
+        <p><a href="#comienzo" title="Comienzo" class="btn">Comienzo...</a></p>
+      </div>
+      <div id="comienzo" class="hidden">
+        <p class="lead">Estando con los ojos abiertos o cerrados, y sin hablar, me concentro en cómo estoy respirando. </p>
+      </div>
+      
+      <div id="estoyRelajado" class="hidden">
+      	<div id="yoestoy">
+      		<p class="lead">Ahora, con cada respiración pienso: <span>"Yo estoy..."</span></p>
+      	</div>
+      	<div id="yoestoy" class="hidden">
+      		<p class="lead">"...relajado"</p>
+      	</div>
+      </div>
+
+    </main><!-- /.container -->
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
